99330978d47def5f5a89a02dab1ac6dace3ed3e9
sarathskumar125 <sarathskumar125@gmail.com>
2023-01-22 22:29:19 +0530

    header and footer created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,116 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Helmet } from "react-helmet-async";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 function App() {
   return (
     <div>
-     <h1>MERN TODO</h1>
+      <Helmet>
+        <title>TODO-APP</title>
+      </Helmet>
+      <Navbar className="bg-info">
+        <Container>
+          <Navbar.Brand href="#home">
+            <h1><strong style={{fontFamily:"initial"}}>TODO APP</strong></h1>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <strong>
+                Signed in as: <a href="#login">Username</a>
+              </strong>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+      <MDBFooter
+        className="text-center text-white footer w-100 bg-info footer"
+      >
+        <MDBContainer className="pt-1" >
+          <section className="mb-1">
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fab fa-facebook-f" />
+            </MDBBtn>
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-twitter" />
+            </MDBBtn>
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-google" />
+            </MDBBtn>
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-instagram" />
+            </MDBBtn>
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-linkedin" />
+            </MDBBtn>
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-github" />
+            </MDBBtn>
+          </section>
+        </MDBContainer>
+        <div
+          className="text-center text-dark p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2023 Copyright:
+          <a className="text-dark" href="#!">
+            MERN-developers
+          </a>
+        </div>
+      </MDBFooter>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
